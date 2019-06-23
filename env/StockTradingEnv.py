dd1f9f2eccb77dc35e3d397710af7ef72283f62a
tejaspolakam <polakam2@illinois.edu>
2019-06-23 15:53:49 -0700

    added methods for step reset and observation

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -13,7 +13,7 @@ class StockTradingEnv(gym.Env):
     # Actions of the format Buy x%, Sell x%, Hold, etc.
     self.action_space = spaces.Box(
       low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
-    
+
     # Prices contains the OHCL values for the last five prices
     self.observation_space = spaces.Box(
       low=0, high=1, shape=(6, 6), dtype=np.float16)
@@ -27,3 +27,5 @@ class StockTradingEnv(gym.Env):
   def render(self, mode='human', close=False):
     # Render the environment to the screen
     ...
+
+    #tindeindienieni
